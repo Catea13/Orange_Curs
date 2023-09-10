f84be35885d933de96a16c9361779d403df27250
Catea <ecaterinah@mail.ru>
2023-09-10 16:28:48 +0300

    Orange Curs

--- src/main/java/Conditions/Task4.java
+++
@@ -1,4 +0,0 @@
-package Conditions;
-
-public class Task4 {
-}
--- src/main/java/ProgramStructure/Task1.java
+++
@@ -1,8 +0,0 @@
-package ProgramStructure;
-//Write a Java program to print 'Hello' on the screen and your name on a separate line. (Without quotes)
-public class Task1 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Catea");
-    }
-}
